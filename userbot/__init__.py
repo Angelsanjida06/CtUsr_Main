fc355d9d3b4fb47102a5c90e42e92e6c6d8c9fac
Angelsanjida06 <98948210+Angelsanjida06@users.noreply.github.com>
2022-03-30 17:20:47 +0600

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -12,8 +12,8 @@ from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
 __version__ = "3.0.6"
 __license__ = "GNU Affero General Public License v3.0"
-__author__ = "CatUserBot <https://github.com/TgCatUB/catuserbot>"
-__copyright__ = f"CatUserBot Copyright (C) 2020 - 2021  {__author__}"
+__author__ = "CatUserBot <https://github.com/Angelsanjida06/CtUsr_Main>"
+__copyright__ = f"AnoBot Copyright (C) 2020 - 2021  {__author__}"
 
 catub.version = __version__
 catub.tgbot.version = __version__
